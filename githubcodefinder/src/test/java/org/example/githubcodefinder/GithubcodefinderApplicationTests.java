de042820f2be81e1ccdddd1bac2115bcda2f8224
msaad809 <muhammadssad70415@gmail.com>
2025-04-24 10:20:35 +0500

    Project added
    Added the project structure

---
+++ githubcodefinder/src/test/java/org/example/githubcodefinder/GithubcodefinderApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.githubcodefinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubcodefinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
